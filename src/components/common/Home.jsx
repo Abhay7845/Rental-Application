c567b0cb809eaf03ed1838da8d7f9b1167d7a4e5
abhay kumar <abhaykr95678@gmail.com>
2023-07-29 15:45:51 +0530

    updated

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import "../../Style/Home.css";
+import axios from "axios";
 
 const Home = () => {
   const [phoneRefrence, setPhoneRefrence] = useState("");
+  const [tableDetails, setTableDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const GetDetails = () => {
+    setLoading(true);
     console.log("phoneRefrence==>", phoneRefrence);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res)
+      .then((response) => {
+        setTableDetails(response.data);
+      })
+      .catch((error) => console.log("error=>", error));
+    setLoading(false);
   };
+
   return (
     <div>
       <Navbar />
@@ -24,31 +38,47 @@ const Home = () => {
             onChange={(e) => setPhoneRefrence(e.target.value)}
           />
           <span className="searchButton" onClick={GetDetails}>
-            Search
+            {loading ? (
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : (
+              <span className="sr-only">Search</span>
+            )}
           </span>
         </div>
       </div>
-      <div className="mx-2 mt-5">
-        <h4 className="text-center mb-3">Table Details</h4>
-        <table class="table table-bordered table-hover border-dark">
-          <thead className="table-dark border-light">
-            <tr>
-              <th>#</th>
-              <th>First</th>
-              <th>Last</th>
-              <th>Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {tableDetails.length > 0 && (
+        <div className="table-responsive mx-2 mt-5">
+          <h4 className="text-center mb-3">Table Details</h4>
+          <table className="table table-bordered table-hover border-dark">
+            <thead className="table-dark border-light">
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Website</th>
+                <th>Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableDetails.map((item, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{item.name}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.email}</td>
+                    <td>{item.website}</td>
+                    <td>{item.address.city}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };

efae7c70ba286ef150d24dfe19185db078b339fc
abhay kumar <abhaykr95678@gmail.com>
2023-07-29 14:26:24 +0530

    updated

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import "../../Style/Home.css";
 
 const Home = () => {
+  const [phoneRefrence, setPhoneRefrence] = useState("");
+  const GetDetails = () => {
+    console.log("phoneRefrence==>", phoneRefrence);
+  };
   return (
     <div>
       <Navbar />
-      <div className="homeBanner">
-        <h3>Rental Apllication and Help</h3>
-        <h3>To Book</h3>
-        <button className="bookBtn">BOOK NOW</button>
+      <div className="row mx-0">
+        <div className="col HomeTextStyle">
+          <h5 className="text-center textH4Style">
+            Find The Perfect Freelance Service <br />
+            For Your Business
+          </h5>
+          <div className="col searchField my-3">
+            <input
+              type="text"
+              className="searchStyle"
+              placeholder="Search by Phone or Refrence No."
+              onChange={(e) => setPhoneRefrence(e.target.value)}
+            />
+            <span className="searchButton" onClick={GetDetails}>
+              Search
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );

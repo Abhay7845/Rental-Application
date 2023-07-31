8f7e3115469e6665e6693c318b77f89def524faa
abhay kumar <abhaykr95678@gmail.com>
2023-07-31 21:58:42 +0530

    updated rental issue

--- src/components/screens/NewBooking.jsx
+++ src/components/screens/NewBooking.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../common/Navbar";
+import { DataList } from "../../Data/DataList";
 
 const NewBooking = () => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <h6 className="TableHeading">Booking Details</h6>
+      <div className="container mt-4">
+        <h6 className="bookingHeading">Booking Details</h6>
         <div className="row g-3">
           <div className="col-md-4">
             <label className="form-label">Reference ID</label>
@@ -28,23 +44,21 @@ const NewBooking = () => {
             <label className="form-label">Booking Date</label>
             <input type="date" className="form-control" />
           </div>
-        </div>
-        <h6 className="TableHeading">Customer Details</h6>
-        <div className="row g-3">
-          <div className="col-md-4">
-            <label className="form-label">Fisr Name</label>
+          <div className="col-md-12">
+            <label className="form-label">Search By Phone or PAN</label>
             <input
               type="type"
               className="form-control"
-              placeholder="Reference ID"
+              placeholder="Search By Phone or PAN"
             />
           </div>
+          <h6 className="bookingHeading mx-2">Customer Details</h6>
           <div className="col-md-4">
-            <label className="form-label">Last Name</label>
+            <label className="form-label">Customer Name</label>
             <input
               type="text"
               className="form-control"
-              placeholder="Store Code"
+              placeholder="Customer Name"
             />
           </div>
           <div className="col-md-4">
@@ -59,6 +73,11 @@ const NewBooking = () => {
             <label className="form-label">Email</label>
             <input type="email" className="form-control" placeholder="Email" />
           </div>
+          <h6 className="bookingHeading mx-2">Customer Address</h6>
+          <div className="col-md-4">
+            <label className="form-label">State</label>
+            <input type="text" className="form-control" placeholder="State" />
+          </div>
           <div className="col-md-4">
             <label className="form-label">City</label>
             <input type="text" className="form-control" placeholder="City" />
@@ -71,7 +90,6 @@ const NewBooking = () => {
               placeholder="Pin Code"
             />
           </div>
-          <h6 className="TableHeading mx-2">Customer Address</h6>
           <div className="col-md-6">
             <label className="form-label">Address Line-1</label>
             <textarea
@@ -88,48 +106,133 @@ const NewBooking = () => {
               placeholder="Address Line-2"
             />
           </div>
-        </div>
-        <div className="p-2" style={{ backgroundColor: "#fac0e1" }}>
-          <h5 className="customer-heading col-md-12">Required Documents:</h5>
-        </div>
-        <div className="card-body">
-          <div>
-            <b>Upload Pancard</b>
-            <input type="file" />
-            <button type="submit">Upload</button>
+          <h6 className="bookingHeading mx-2">Required Documents</h6>
+          <div className="col-md-6">
+            <label className="form-label">Addeess ID Proof Type</label>
+            <select className="form-control">
+              <option>Select Type</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
           </div>
-        </div>
-        <div className="p-2" style={{ backgroundColor: "#fac0e1" }}>
-          <h5 className="customer-heading col-md-12">Product Details:</h5>
-        </div>
-        <div
-          className="card-body table-resposive"
-          style={{ maxHeight: "400px", overflowY: "auto" }}
-        >
-          <table className="table table-hover border table-bordered">
-            <thead className="table-light">
-              <tr>
-                <th>Item Code</th>
-                <th>Lot Number</th>
-                <th>Rent Start Date</th>
-                <th>Package Days</th>
-                <th>Product Value</th>
-                <th>Rental Amount</th>
-                <th>Deposit Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="col-md-6">
+            <label className="form-label">Customer Type</label>
+            <select className="form-control">
+              <option>Select Type</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+          </div>
+          <div className="col-md-4">
+            <label className="form-label">Upload PAN</label>
+            <input
+              type="file"
+              className="form-control"
+              onChange={handleImageChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label className="form-label">Address Proof ID No.</label>
+            <input
+              type="file"
+              className="form-control"
+              onChange={handleImageChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label className="form-label">Customer Previous Trasaction</label>
+            <input
+              type="file"
+              className="form-control"
+              onChange={handleImageChange}
+            />
+          </div>
+          <div className="col-md-4 d-flex justify-content-center">
+            {image && (
+              <img src={image} alt="Preview" height="100px" width="200px" />
+            )}
+          </div>
+          <div className="col-md-4 d-flex justify-content-center">
+            {image && (
+              <img src={image} alt="Preview" height="100px" width="200px" />
+            )}
+          </div>
+          <div className="col-md-4 d-flex justify-content-center">
+            {image && (
+              <img src={image} alt="Preview" height="100px" width="200px" />
+            )}
+          </div>
+          <div className="col-md-4 d-flex justify-content-between">
+            <label className="form-label">With in Catchment?</label>
+            <div>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="gridRadios"
+                defaultChecked
+              />
+              <label className="form-check-label mx-2">YES</label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="gridRadios"
+                defaultChecked
+              />
+              <label className="form-check-label mx-2">NO</label>
+            </div>
+          </div>
+          {DataList.length > 0 && (
+            <div className="col-12">
+              <h6 className="bookingHeading">Item Details</h6>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover border-dark">
+                  <thead className="table-dark border-light">
+                    <tr>
+                      <th>Item Code</th>
+                      <th>Lot No.</th>
+                      <th>Package_Days</th>
+                      <th>Actual_Weight </th>
+                      <th>Product_Value</th>
+                      <th>Rental_Amount</th>
+                      <th>Deposit_Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {DataList.map((item, i) => {
+                      return (
+                        <tr key={i}>
+                          <td className="text-center border-dark">
+                            IKFDSVAKFVKNRESC
+                          </td>
+                          <td>{item.name}</td>
+                          <td>{item.phone}</td>
+                          <td>{item.email}</td>
+                          <td>{item.website}</td>
+                          <td>{item.address.city}</td>
+                          <td>{item.address.city}</td>
+                        </tr>
+                      );
+                    })}
+                    <tr>
+                      <th colSpan="4" className="text-end">
+                        TOTAL
+                      </th>
+                      <th>234</th>
+                      <th>124</th>
+                      <th>678</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+          <div className="d-flex justify-content-end mb-4">
+            <button type="button" className="CButton">
+              NEXT
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/screens/RentalIssue.jsx
+++ src/components/screens/RentalIssue.jsx
@@ -22,9 +22,9 @@ const RentalIssue = () => {
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <h6 className="TableHeading">Rental Issue</h6>
-        <div className="row g-3">
+      <div className="container mt-4">
+        <h6 className="bookingHeading">Rental Issue</h6>
+        <div className="row g-3 mb-4">
           <div className="col-md-4">
             <label className="form-label">Booking Ref No</label>
             <input
@@ -109,172 +109,181 @@ const RentalIssue = () => {
               </div>
             </div>
           </div>
-        </div>
-        {DataList.length > 0 && (
-          <div>
-            <h6 className="TableHeading">Item Details</h6>
-            <div className="table-responsive">
-              <table className="table table-bordered table-hover border-dark">
-                <thead className="table-dark border-light">
-                  <tr>
-                    <th>Item Code</th>
-                    <th>Lot No.</th>
-                    <th>Package_Days</th>
-                    <th>Actual_Weight </th>
-                    <th>Product_Value</th>
-                    <th>Rental_Amount</th>
-                    <th>Deposit_Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {DataList.map((item, i) => {
-                    return (
-                      <tr key={i}>
-                        <td className="text-center border-dark">
-                          IKFDSVAKFVKNRESC
-                        </td>
-                        <td>{item.name}</td>
-                        <td>{item.phone}</td>
-                        <td>{item.email}</td>
-                        <td>{item.website}</td>
-                        <td>{item.address.city}</td>
-                        <td>{item.address.city}</td>
-                      </tr>
-                    );
-                  })}
-                  <tr>
-                    <th colSpan="4" className="text-end">
-                      TOTAL
-                    </th>
-                    <th>234</th>
-                    <th>124</th>
-                    <th>678</th>
-                  </tr>
-                </tbody>
-              </table>
+
+          {DataList.length > 0 && (
+            <div className="col-12">
+              <h6 className="bookingHeading">Item Details</h6>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover border-dark">
+                  <thead className="table-dark border-light">
+                    <tr>
+                      <th>Item Code</th>
+                      <th>Lot No.</th>
+                      <th>Package_Days</th>
+                      <th>Actual_Weight </th>
+                      <th>Product_Value</th>
+                      <th>Rental_Amount</th>
+                      <th>Deposit_Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {DataList.map((item, i) => {
+                      return (
+                        <tr key={i}>
+                          <td className="text-center border-dark">
+                            IKFDSVAKFVKNRESC
+                          </td>
+                          <td>{item.name}</td>
+                          <td>{item.phone}</td>
+                          <td>{item.email}</td>
+                          <td>{item.website}</td>
+                          <td>{item.address.city}</td>
+                          <td>{item.address.city}</td>
+                        </tr>
+                      );
+                    })}
+                    <tr>
+                      <th colSpan="4" className="text-end">
+                        TOTAL
+                      </th>
+                      <th>234</th>
+                      <th>124</th>
+                      <th>678</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
-        )}
-        {DataList.length > 0 && (
-          <div>
-            <h6 className="TableHeading">Deposit Amount Payment Details</h6>
-            <div className="table-responsive">
-              <table className="table table-bordered table-hover border-dark">
-                <thead className="table-dark border-light">
-                  <tr>
-                    <th>Type</th>
-                    <th>Ref No.</th>
-                    <th>Amount</th>
-                    <th>Upload</th>
-                    <th>View</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {DataList.map((item, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>
-                          <select className="w-100">
-                            <option>Slect Type</option>
-                            <option>Creadit Note</option>
-                            <option>Creadit Card</option>
-                          </select>
-                        </td>
-                        <td>
-                          <input
-                            type="text"
-                            className="w-100"
-                            placeholder="Ref Number"
-                          />
-                        </td>
-                        <td>
-                          <input
-                            type="text"
-                            className="w-100"
-                            placeholder="Amount"
-                          />
-                        </td>
-                        <td className="d-flex justify-content-center">
-                          <input
-                            type="file"
-                            onChange={handleImageChange}
-                            style={{ cursor: "pointer" }}
-                          />
-                        </td>
-                        <td>
-                          {image && (
-                            <img
-                              src={image}
-                              alt="Preview"
-                              height="80px"
-                              width="100%"
+          )}
+
+          {DataList.length > 0 && (
+            <div className="col-12">
+              <h6 className="bookingHeading">Deposit Amount Payment Details</h6>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover border-dark">
+                  <thead className="table-dark border-light">
+                    <tr>
+                      <th>Type</th>
+                      <th>Ref No.</th>
+                      <th>Amount</th>
+                      <th>Upload</th>
+                      <th>View</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {DataList.map((item, i) => {
+                      return (
+                        <tr key={i}>
+                          <td>
+                            <select className="w-100">
+                              <option>Slect Type</option>
+                              <option>Creadit Note</option>
+                              <option>Creadit Card</option>
+                            </select>
+                          </td>
+                          <td>
+                            <input
+                              type="text"
+                              className="w-100"
+                              placeholder="Ref Number"
                             />
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                  <tr>
-                    <td colSpan="2" className="text-end">
-                      Total Deposit Amount Paid
-                    </td>
-                    <td>
-                      <input type="text" value={6} className="w-100" disabled />
-                    </td>
-                    <td colSpan="2" />
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        )}
-        {DataList.length > 0 && (
-          <div>
-            <h6 className="TableHeading">Delivery Inspection Product Images</h6>
-            <div className="table-responsive">
-              <table className="table table-bordered table-hover border-dark">
-                <thead className="table-dark border-light text-center">
-                  <tr>
-                    <th>Item Code</th>
-                    <th>Upload</th>
-                    <th>View</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {DataList.map((item, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>ASFVFGHYHEY</td>
-                        <td className="text-center">
-                          <input
-                            type="file"
-                            onChange={handleImageChange}
-                            style={{ cursor: "pointer" }}
-                          />
-                        </td>
-                        <td>
-                          {image && (
-                            <img
-                              src={image}
-                              alt="Preview"
-                              height="80px"
-                              width="100%"
+                          </td>
+                          <td>
+                            <input
+                              type="text"
+                              className="w-100"
+                              placeholder="Amount"
                             />
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+                          </td>
+                          <td className="d-flex justify-content-center">
+                            <input
+                              type="file"
+                              onChange={handleImageChange}
+                              style={{ cursor: "pointer" }}
+                            />
+                          </td>
+                          <td>
+                            {image && (
+                              <img
+                                src={image}
+                                alt="Preview"
+                                height="80px"
+                                width="100%"
+                              />
+                            )}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                    <tr>
+                      <td colSpan="2" className="text-end">
+                        Total Deposit Amount Paid
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          value={6}
+                          className="w-100"
+                          disabled
+                        />
+                      </td>
+                      <td colSpan="2" />
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+          )}
+          {DataList.length > 0 && (
+            <div className="col-12">
+              <h6 className="bookingHeading">
+                Delivery Inspection Product Images
+              </h6>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover border-dark">
+                  <thead className="table-dark border-light text-center">
+                    <tr>
+                      <th>Item Code</th>
+                      <th>Upload</th>
+                      <th>View</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {DataList.map((item, i) => {
+                      return (
+                        <tr key={i}>
+                          <td>ASFVFGHYHEY</td>
+                          <td className="text-center">
+                            <input
+                              type="file"
+                              onChange={handleImageChange}
+                              style={{ cursor: "pointer" }}
+                            />
+                          </td>
+                          <td>
+                            {image && (
+                              <img
+                                src={image}
+                                alt="Preview"
+                                height="80px"
+                                width="100%"
+                              />
+                            )}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+          <div className="col-12">
+            <h6 className="bookingHeading">
+              Print Delivery Inspection Acknowledgement
+            </h6>
           </div>
-        )}
-        <h6 className="TableHeading">
-          Print Delivery Inspection Acknowledgement
-        </h6>
-        <div className="row g-3">
-          <div className="col-md-3 d-flex justify-content-between">
+          <div className="col-md-3 d-flex justify-content-between mt-0">
             <label className="form-label">Booking Ref No</label>
             <div>
               <input
@@ -295,7 +304,7 @@ const RentalIssue = () => {
               <label className="form-check-label mx-2">NO</label>
             </div>
           </div>
-          <div className="col-md-9 d-flex justify-content-between">
+          <div className="col-md-9 d-flex justify-content-between mt-0">
             <div className="row w-100 g-2">
               <label>Loan Document Reference</label>
               <div className="col-md-4">
@@ -308,24 +317,24 @@ const RentalIssue = () => {
               </div>
             </div>
           </div>
-        </div>
-        <h6 className="TableHeading">
-          Signed Acknowledgement of Product Received after Inspection
-        </h6>
-        <div className="row g-3">
-          <div className="col-md-3">
+          <div className="col-12">
+            <h6 className="bookingHeading">
+              Signed Acknowledgement of Product Received after Inspection
+            </h6>
+          </div>
+          <div className="col-md-3 mt-0">
             <input type="file" className="form-control" />
           </div>
-          <div className="col-md-2">
+          <div className="col-md-2 mt-0">
             {image && (
               <img src={image} alt="Preview" height="80px" width="100%" />
             )}
           </div>
-        </div>
-        <div className="d-flex justify-content-end mb-4">
-          <button type="button" className="CButton">
-            Print
-          </button>
+          <div className="d-flex justify-content-end mb-4">
+            <button type="button" className="CButton">
+              Print
+            </button>
+          </div>
         </div>
       </div>
     </div>

0182c7c6e9f131b8cb2f3edeff810c326a5d0492
abhay kumar <abhaykr95678@gmail.com>
2023-08-02 15:58:18 +0530

    updated

--- src/components/screens/RentaRetrun.jsx
+++ src/components/screens/RentaRetrun.jsx
@@ -2,20 +2,47 @@ import React, { useState } from "react";
 import Navbar from "../common/Navbar";
 import "../../Style/RentalIssue.css";
 import { DataList } from "../../Data/DataList";
+import { BsFillTrashFill, BsFillEyeFill } from "react-icons/bs";
 
-const RentalRetrun = () => {
-  const [image, setImage] = useState(null);
+const RentalIssue = () => {
+  // DELIVERY INSPECTION ADD ROWS
+  const [deliveryRowCont, setDeliveryRowCont] = useState(0);
+  const [addDeliveryItems, setAddDeliveryItems] = useState([]);
+  const [deliveryProductId, setDeliveryProductId] = useState(0);
+  const [addDeliveryProducts, setAddDeliveryProducts] = useState([]);
 
-  const handleImageChange = (event) => {
+  // DELIVERY INSPECTION PRODUCTS INPUT VALUES
+  const [deliveryProductItemCode, setDeliveryProductItemCode] = useState("");
+  const [deliveryProductFile, setDeliveryProductImg] = useState(null);
+
+  const AddDeliveryRowsInputs = () => {
+    setDeliveryRowCont(deliveryRowCont + 1);
+    setAddDeliveryItems([...addDeliveryItems, deliveryRowCont + 1]);
+  };
+
+  const UploadDeliveryProductImg = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-
     reader.onloadend = () => {
-      setImage(reader.result);
+      setDeliveryProductImg(reader.result);
     };
-
     if (file) {
       reader.readAsDataURL(file);
+    }
+  };
+
+  const SaveItemsDetails = () => {
+    if (!deliveryProductItemCode) {
+      alert("Please Enter All Details");
+    } else {
+      setDeliveryProductId(deliveryProductId + 1);
+      const deliveryProductsTable = {
+        id: deliveryRowCont,
+        itemCode: deliveryProductItemCode,
+        delieryProductFile: deliveryProductFile,
+      };
+      setAddDeliveryProducts([...addDeliveryProducts, deliveryProductsTable]);
+      setAddDeliveryItems([]);
     }
   };
 
@@ -54,30 +81,28 @@ const RentalRetrun = () => {
               <option value="3">2</option>
             </select>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-4">
             <label className="form-label">Customer ID No</label>
             <input
               type="text"
               className="form-control"
-              placeholder="Customer ID No"
+              placeholder="Customer ID No."
             />
           </div>
-          <div className="col-md-3">
+          <div className="col-md-4">
             <label className="form-label">Upload ID</label>
             <input
               type="file"
               className="form-control"
-              onChange={handleImageChange}
+              onChange={UploadDeliveryProductImg}
             />
           </div>
-          <div className="col-md-2">
-            {image && (
-              <img
-                src={image}
-                alt="Preview"
-                className="fullScreenImage"
+          <div className="col-12 d-flex justify-content-end mt-1">
+            {deliveryProductFile && (
+              <BsFillEyeFill
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
+                style={{ cursor: "pointer" }}
               />
             )}
             <div
@@ -97,9 +122,9 @@ const RentalRetrun = () => {
                     />
                   </div>
                   <div className="modal-body">
-                    {image && (
+                    {deliveryProductFile && (
                       <img
-                        src={image}
+                        src={deliveryProductFile}
                         alt="Preview"
                         className="fullScreenImage"
                       />
@@ -176,7 +201,7 @@ const RentalRetrun = () => {
                         <tr key={i}>
                           <td>
                             <select className="w-100">
-                              <option>Slect Type</option>
+                              <option>Select Type</option>
                               <option>Creadit Note</option>
                               <option>Creadit Card</option>
                             </select>
@@ -198,14 +223,14 @@ const RentalRetrun = () => {
                           <td className="d-flex justify-content-center">
                             <input
                               type="file"
-                              onChange={handleImageChange}
+                              onChange={UploadDeliveryProductImg}
                               style={{ cursor: "pointer" }}
                             />
                           </td>
                           <td>
-                            {image && (
+                            {deliveryProductFile && (
                               <img
-                                src={image}
+                                src={deliveryProductFile}
                                 alt="Preview"
                                 height="80px"
                                 width="100%"
@@ -236,45 +261,72 @@ const RentalRetrun = () => {
           )}
           {DataList.length > 0 && (
             <div className="col-12">
-              <h6 className="bookingHeading">
-                Delivery Inspection Product Images
-              </h6>
+              <h6 className="bookingHeading">Delivery Inspection Product</h6>
               <div className="table-responsive">
                 <table className="table table-bordered table-hover border-dark">
                   <thead className="table-dark border-light text-center">
                     <tr>
                       <th>Item Code</th>
-                      <th>Upload</th>
                       <th>View</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {DataList.map((item, i) => {
+                    {addDeliveryProducts.map((item, i) => {
                       return (
                         <tr key={i}>
-                          <td>ASFVFGHYHEY</td>
+                          <td>{item.itemCode}</td>
                           <td className="text-center">
-                            <input
-                              type="file"
-                              onChange={handleImageChange}
-                              style={{ cursor: "pointer" }}
-                            />
-                          </td>
-                          <td>
-                            {image && (
-                              <img
-                                src={image}
-                                alt="Preview"
-                                height="80px"
-                                width="100%"
-                              />
-                            )}
+                            <BsFillEyeFill />
                           </td>
                         </tr>
                       );
                     })}
+
+                    {addDeliveryItems.length > 0 && (
+                      <tr>
+                        <td>
+                          <input
+                            type="text"
+                            placeholder="Item Code"
+                            className="w-100"
+                            onChange={(e) =>
+                              setDeliveryProductItemCode(e.target.value)
+                            }
+                          />
+                        </td>
+                        <td className="d-flex justify-content-between">
+                          <input
+                            type="file"
+                            onChange={UploadDeliveryProductImg}
+                          />
+                          <BsFillTrashFill
+                            className="DeleteRow"
+                            onClick={() => setAddDeliveryItems([])}
+                          />
+                        </td>
+                      </tr>
+                    )}
                   </tbody>
                 </table>
+                <div className="d-flex justify-content-end mt-0">
+                  {addDeliveryItems.length > 0 ? (
+                    <button
+                      type="submit"
+                      className="CButton"
+                      onClick={SaveItemsDetails}
+                    >
+                      Save Row
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="CButton"
+                      onClick={AddDeliveryRowsInputs}
+                    >
+                      Add Row
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           )}
@@ -283,39 +335,35 @@ const RentalRetrun = () => {
               Print Delivery Inspection Acknowledgement
             </h6>
           </div>
-          <div className="col-md-3 d-flex justify-content-between mt-0">
-            <label className="form-label">Booking Ref No</label>
-            <div>
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gridRadios"
-                defaultChecked
-              />
-              <label className="form-check-label mx-2">YES</label>
+          <div className="row d-flex">
+            <div className="col-md-4 d-flex">
+              <label className="form-label">Booking Ref No.</label>
+              <div className="mx-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="t&c"
+                  defaultChecked
+                />
+                <label className="form-check-label mx-1">YES</label>
+              </div>
+              <div>
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="t&c"
+                  defaultChecked
+                />
+                <label className="form-check-label mx-1">NO</label>
+              </div>
             </div>
-            <div>
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gridRadios"
-                defaultChecked
-              />
-              <label className="form-check-label mx-2">NO</label>
+            <div className="col-md-8 d-flex justify-content-between">
+              <label className="form-label col-md-2">Loan Documents</label>
+              <input type="file" className="form-control" />
             </div>
           </div>
-          <div className="col-md-9 d-flex justify-content-between mt-0">
-            <div className="row w-100 g-2">
-              <label>Loan Document Reference</label>
-              <div className="col-md-4">
-                <input type="file" className="form-control" />
-              </div>
-              <div className="col-md-2">
-                {image && (
-                  <img src={image} alt="Preview" height="80px" width="100%" />
-                )}
-              </div>
-            </div>
+          <div className="d-flex justify-content-end">
+            <button className="CButton">Print</button>
           </div>
           <div className="col-12">
             <h6 className="bookingHeading">
@@ -326,13 +374,18 @@ const RentalRetrun = () => {
             <input type="file" className="form-control" />
           </div>
           <div className="col-md-2 mt-0">
-            {image && (
-              <img src={image} alt="Preview" height="80px" width="100%" />
+            {deliveryProductFile && (
+              <img
+                src={deliveryProductFile}
+                alt="Preview"
+                height="80px"
+                width="100%"
+              />
             )}
           </div>
           <div className="d-flex justify-content-end mb-4">
             <button type="button" className="CButton">
-              Print
+              Save
             </button>
           </div>
         </div>
@@ -341,4 +394,4 @@ const RentalRetrun = () => {
   );
 };
 
-export default RentalRetrun;
+export default RentalIssue;

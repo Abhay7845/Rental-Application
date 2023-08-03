d639593cc06528b6715b90947a5147ac7d32def1
abhay kumar <abhaykr95678@gmail.com>
2023-08-03 23:12:42 +0530

    updated

--- src/components/screens/RentalIssue.jsx
+++ src/components/screens/RentalIssue.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../common/Navbar";
 import "../../Style/RentalIssue.css";
-import { DataList } from "../../Data/DataList";
+import { DataList, packageDays } from "../../Data/DataList";
 import { BsFillTrashFill, BsFillEyeFill } from "react-icons/bs";
 
 const RentalIssue = () => {
@@ -77,6 +77,22 @@ const RentalIssue = () => {
               placeholder="Customer Name"
             />
           </div>
+          <div className="col-md-6">
+            <label className="form-label">Phone Number</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Phone Number"
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Email ID</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email ID"
+            />
+          </div>
           <div className="col-md-4">
             <label className="form-label">Customer ID Type</label>
             <select className="form-control">
@@ -87,7 +103,7 @@ const RentalIssue = () => {
             </select>
           </div>
           <div className="col-md-4">
-            <label className="form-label">Customer ID No</label>
+            <label className="form-label">Customer ID No.</label>
             <input
               type="text"
               className="form-control"
@@ -113,35 +129,69 @@ const RentalIssue = () => {
               onChange={UploadDeliveryProductImg}
             />
           </div>
-          <div className="col-12 d-flex justify-content-end mt-1">
-            <div
-              className="modal fade"
-              id="exampleModal"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog modal-xl">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    />
-                  </div>
-                  <div className="modal-body">
-                    {deliveryProductFile && (
-                      <img
-                        src={deliveryProductFile}
-                        alt="Preview"
-                        className="fullScreenImage"
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
+          <div className="col-md-12">
+            <b>Same Customer Pickup</b>
+            <input class="form-check-input mx-3" type="checkbox" />
+          </div>
+          <div className="col-md-3">
+            <label className="form-label">Customer Name</label>
+            <input
+              type="number"
+              disabled
+              className="form-control"
+              placeholder="Customer Name"
+            />
+          </div>
+          <div className="col-md-3">
+            <label className="form-label">Customer ID Type</label>
+            <select className="form-control" disabled>
+              <option value="Type">Type</option>
+            </select>
+          </div>
+          <div className="col-md-3">
+            <label className="form-label">Customer ID No.</label>
+            <input
+              type="text"
+              disabled
+              className="form-control"
+              placeholder="Customer ID No."
+            />
+          </div>
+          <div className="col-md-3">
+            <div className="d-flex justify-content-between">
+              <label className="form-label">Upload ID</label>
+              <span className="mx-2">
+                {deliveryProductFile && (
+                  <BsFillEyeFill
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    style={{ cursor: "pointer" }}
+                  />
+                )}
+              </span>
             </div>
+            <input
+              type="file"
+              className="form-control"
+              onChange={UploadDeliveryProductImg}
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Rental Start Date</label>
+            <input type="date" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Package Days</label>
+            <select type="date" className="form-control">
+              <option>Select Days</option>
+              {packageDays.map((days, i) => {
+                return (
+                  <option key={i} value={days}>
+                    {days}
+                  </option>
+                );
+              })}
+            </select>
           </div>
 
           {DataList.length > 0 && (
@@ -151,44 +201,47 @@ const RentalIssue = () => {
                 <table className="table table-bordered table-hover border-dark">
                   <thead className="table-dark border-light">
                     <tr>
-                      <th>Item Code</th>
+                      <th>Item_Code</th>
                       <th>Lot No.</th>
-                      <th>Package_Days</th>
+                      <th>No. Of PCS</th>
+                      <th>PCS No.</th>
+                      <th>HUID</th>
+                      <th>CFA</th>
+                      <th>Gross_Weight</th>
                       <th>Product_Value</th>
                       <th>Rental_Amount</th>
                       <th>Deposit_Amount</th>
-                      <th>Actual_Weight </th>
+                      <th>Actual_Weight</th>
                     </tr>
                   </thead>
                   <tbody>
                     {DataList.map((item, i) => {
                       return (
                         <tr key={i}>
-                          <td className="text-center border-dark">
-                            IKFDSVAKFVKNRESC
-                          </td>
-                          <td>{item.name}</td>
-                          <td>{item.phone}</td>
-                          <td>{item.email}</td>
-                          <td>{item.website}</td>
-                          <td>{item.address.city}</td>
+                          <td>IKFDSVAKF</td>
+                          <td>23</td>
+                          <td>54</td>
+                          <td>34</td>
+                          <td>12</td>
+                          <td>7</td>
+                          <td>12</td>
+                          <td>6</td>
+                          <td>43</td>
+                          <td>2</td>
                           <td>
-                            <input
-                              type="number"
-                              placeholder="Actual Weight"
-                              className="w-100"
-                            />
+                            <input type="number" placeholder="Actual Weight" />
                           </td>
                         </tr>
                       );
                     })}
                     <tr>
-                      <th colSpan="4" className="text-end">
+                      <th colSpan="7" className="text-end">
                         TOTAL
                       </th>
                       <th>234</th>
                       <th>124</th>
                       <th>678</th>
+                      <th colSpan="1" />
                     </tr>
                   </tbody>
                 </table>
@@ -406,6 +459,36 @@ const RentalIssue = () => {
             <button type="button" className="CButton">
               Save
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 d-flex justify-content-end mt-1">
+        <div
+          className="modal fade"
+          id="exampleModal"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-xl">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">
+                {deliveryProductFile && (
+                  <img
+                    src={deliveryProductFile}
+                    alt="Preview"
+                    className="fullScreenImage"
+                  />
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>

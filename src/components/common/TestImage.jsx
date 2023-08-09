9f49dd625e9e89785744650c5df4718ec49c2945
abhay kumar <abhaykr95678@gmail.com>
2023-08-09 12:34:48 +0530

    updated

--- src/components/common/TestImage.jsx
+++ src/components/common/TestImage.jsx
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 
 const TestImage = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  console.log("imageUrl==>", imageUrl);
-  const handleImageUpload = async (event) => {
+  //   const [imageUrl, setImageUrl] = useState("");
+  const UploadFile = (event) => {
     const file = event.target.files[0];
-
     const formData = new FormData();
-    formData.append("image", file);
-
-    try {
-      const response = await axios.post("/upload", formData); // Replace with your server endpoint
-      const generatedImageUrl = response.data.imageUrl;
-      setImageUrl(generatedImageUrl);
-    } catch (error) {
-      console.error("Error uploading image:", error);
-    }
+    formData.append(file.name, file);
+    axios
+      .post("https://jewbridge.titanjew.in/NPD/API/IMAGEFETCH", formData, {
+        headers: {
+          UserToken: "xFeToMkUuejH0aq1IzZYmw==",
+          ApiKey: "636A4E75-2B3D-4B83-8DD6-F36046290E0F",
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) =>
+        res.then((response) => console.log("response==>", response))
+      )
+      .catch((error) => console.log("error==>", error));
   };
   return (
     <div>
       <Navbar />
       <div>
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-        {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+        <input type="file" onChange={UploadFile} />
       </div>
     </div>
   );

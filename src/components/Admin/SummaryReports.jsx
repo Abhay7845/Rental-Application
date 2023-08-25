ae0911414efbad1b7a2645a0720a531aba223729
abhay kumar <abhaykr95678@gmail.com>
2023-08-25 11:52:21 +0530

    updated

--- src/components/Admin/SummaryReports.jsx
+++ src/components/Admin/SummaryReports.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import Navbar from "../common/Navbar";
+import { DataList } from "../../Data/DataList";
 
 const SummaryReports = () => {
   return (
     <div>
       <Navbar />
-      SummaryReports
+      <div className="row mx-0 mt-3">
+        <h5 className="text-center">SUMMARY REPORTS</h5>
+        <div className="col-12 table-responsive mx-0">
+          <table className="table table-bordered table-hover border-dark">
+            <thead className="table-dark border-light">
+              <tr>
+                <th>Item Code</th>
+                <th>Image</th>
+                <th>Std Wt</th>
+                <th>Std UCP</th>
+                <th>Std_Rental_Value</th>
+                <th>Std_Deposit_Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {DataList.map((item, i) => {
+                return (
+                  <tr key={i}>
+                    <td>IKFDSVAKFVKNRESC</td>
+                    <td>{item.name}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.email}</td>
+                    <td>{item.website}</td>
+                    <td>{item.address.city}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };

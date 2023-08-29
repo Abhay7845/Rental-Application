77f5a4c04edad13275850481304170b1161054e3
abhay kumar <abhaykr95678@gmail.com>
2023-08-29 15:58:31 +0530

    updated

--- src/components/RSO/ProductsDetails.jsx
+++ src/components/RSO/ProductsDetails.jsx
@@ -1,16 +1,86 @@
 import React, { useState } from "react";
 import Navbar from "../common/Navbar";
-import { DataList, packageDayOption } from "../../Data/DataList";
+import { packageDayOption } from "../../Data/DataList";
+import axios from "axios";
+import { HOST_URL } from "../../API/HostURL";
+import Loader from "../common/Loader";
 
 const ProductsDetails = () => {
   const [rentalDate, setRentalDate] = useState("");
   const [packageDays, setPackageDays] = useState("");
-  console.log("rentalDate==>", rentalDate);
+  const [itemCode, setItemCode] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [productDetails, setProductDetails] = useState({});
+  const storeCode = localStorage.getItem("rsoRole");
+
+  const GetProductDetails = () => {
+    setLoading(true);
+    axios
+      .get(`${HOST_URL}/rental/product/view/details/MAMTHA/${itemCode}`)
+      .then((res) => res)
+      .then((response) => {
+        if (response.data.code === "1000") {
+          setProductDetails(response.data.value);
+        }
+        if (response.data.code === "1001") {
+          console.log("daata");
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  };
+
+  const CheckAvaiblity = () => {
+    setLoading(true);
+    const CheckAvaiblity = {
+      bookedDate: "",
+      bookingDate: rentalDate,
+      endDate: "",
+      image: "",
+      itemCode: itemCode,
+      packageDays: packageDays,
+      stdUCP: "",
+      stdWt: "",
+      storeCode: storeCode,
+    };
+    console.log("CheckAvaiblity==>", CheckAvaiblity);
+    axios
+      .post(`${HOST_URL}/check/item/availability`, CheckAvaiblity)
+      .then((res) => res)
+      .then((response) => {
+        if (response.data.code === "1000") {
+          if (response.data.value === "Available") {
+            GetProductDetails();
+          } else {
+            alert("Product Not Available");
+          }
+        }
+        if (response.data.code === "1001") {
+          console.log(response.data.value);
+        }
+        setLoading(false);
+      })
+      .catch((errro) => {
+        setLoading(false);
+      });
+  };
   return (
     <div>
       <Navbar />
+      {loading === true && <Loader />}
       <div className="row g-3 mx-0 mt-3">
-        <div className="col-md-5">
+        <div className="col-md-3">
+          <label className="form-label">Item Code</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Item Code"
+            onChange={(e) => setItemCode(e.target.value)}
+          />
+        </div>
+        <div className="col-md-3">
           <label className="form-label">Rent Start Date</label>
           <input
             type="date"
@@ -19,7 +89,7 @@ const ProductsDetails = () => {
             onChange={(e) => setRentalDate(e.target.value)}
           />
         </div>
-        <div className="col-md-5">
+        <div className="col-md-3">
           <label className="form-label">Package Days</label>
           <select
             className="form-control"
@@ -39,37 +109,41 @@ const ProductsDetails = () => {
         <div className="col-md-2">
           <label className="form-label">.</label>
           <div className="d-flex justify-content-end">
-            <button className="CButton">Check Availability</button>
+            <button className="CButton" onClick={CheckAvaiblity}>
+              Check Availability
+            </button>
           </div>
         </div>
-        <div className="col-12 table-responsive">
-          <table className="table table-bordered table-hover border-dark">
-            <thead className="table-dark border-light">
-              <tr>
-                <th>Item Code</th>
-                <th>Image</th>
-                <th>Std Wt</th>
-                <th>Std UCP</th>
-                <th>Std_Rental_Value</th>
-                <th>Std_Deposit_Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {DataList.map((item, i) => {
-                return (
-                  <tr key={i}>
-                    <td>IKFDSVAKFVKNRESC</td>
-                    <td>{item.name}</td>
-                    <td>{item.phone}</td>
-                    <td>{item.email}</td>
-                    <td>{item.website}</td>
-                    <td>{item.address.city}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        {productDetails.pdtID && (
+          <div className="col-12 table-responsive">
+            <table className="table table-bordered table-hover border-dark">
+              <thead className="table-dark border-light">
+                <tr>
+                  <th>PdtID</th>
+                  <th>HUID</th>
+                  <th>Item Code</th>
+                  <th>Lot No.</th>
+                  <th>CFA</th>
+                  <th>Gross Wt</th>
+                  <th>Net Wt</th>
+                  <th>Product Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{productDetails.pdtID}</td>
+                  <td>{productDetails.huID}</td>
+                  <td>{productDetails.itemCode}</td>
+                  <td>{productDetails.lotNo}</td>
+                  <td>{productDetails.cfa}</td>
+                  <td>{productDetails.grossWt}</td>
+                  <td>{productDetails.netWt}</td>
+                  <td>{productDetails.productValue}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   );

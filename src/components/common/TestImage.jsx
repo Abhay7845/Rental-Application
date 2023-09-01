8efa0b19aeeb29f4a2c352a6a12eba52f1b75288
abhay kumar <abhaykr95678@gmail.com>
2023-09-01 14:17:53 +0530

    updatwd'

--- src/components/common/TestImage.jsx
+++ src/components/common/TestImage.jsx
@@ -1,82 +1,244 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
-import axios from "axios";
-import { UploadImg } from "../../API/HostURL";
-import { useNavigate } from "react-router-dom";
+import { WishListHeader } from "../../Data/DataList";
 
 const TestImage = () => {
-  const [imageName, setImageName] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const navigate = useNavigate();
-  const UploadFile = (event) => {
-    const file = event.target.files[0];
-    const fileExtention = file.name.split(".");
-    console.log("fileExtention==>", fileExtention[1]);
-    const formData = new FormData();
-    formData.append("ImgName", `NNNNN1234N432345.${fileExtention[1]}`);
-    formData.append("files", file);
-    axios
-      .post(`${UploadImg}`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          UserToken: "xFeToMkUuejH0aq1IzZYmw==",
-          ApiKey: "636A4E75-2B3D-4B83-8DD6-F36046290E0F",
-        },
-      })
-      .then((res) => res)
-      .then((response) => {
-        console.log("responseUpload==>", response.data);
-        if (response.data) {
-          setImageName("NNNNN1234N432345");
-        }
-      })
-      .catch((error) => console.log("error==>", error));
+  const handlePrint = () => {
+    window.print();
   };
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://jewbridge.titanjew.in/NPD/api/Docfetch/DownloadImage/?ImageName=NNNNN1234N432345.png`,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            UserToken: "xFeToMkUuejH0aq1IzZYmw==",
-            ApiKey: "636A4E75-2B3D-4B83-8DD6-F36046290E0F",
-          },
-        }
-      )
-      .then((res) => res)
-      .then((response) => {
-        console.log("responseFetch==>", response.data);
-        if (response.data) {
-          setImageUrl(response.data);
-        }
-      })
-      .catch((error) => console.log("error=>", error));
-  }, [imageName]);
-
-  const CheckUserRegistered = () => {
-    const result = window.confirm("Customer not Registred Please Resgitred");
-    if (result) {
-      navigate("/new/customer");
-    } else {
-      console.log("User clicked Cancel");
-    }
-  };
-
   return (
     <div>
       <Navbar />
-      <input type="file" onChange={UploadFile} capture="image" />
-      {imageUrl && (
-        <img
-          src={`data:image/jpeg;base64,${imageUrl}`}
-          // src={imageUrl}
-          alt="imageUrl"
-          height="100"
-        />
-      )}
-      <button onClick={CheckUserRegistered}>Show Alert</button>
+      <button onClick={handlePrint} className="print-button">
+        Print
+      </button>
+      <div className="table-container hide-on-screen">
+        <h6 className="text-center">
+          BOOKING ORDER CONFIRMATION/ADVANCE RECEIPT
+        </h6>
+        <style>
+          {`
+          @media screen{
+            .hide-on-screen{
+              display:block;
+            }
+          }
+          @media print {
+            .print-button {
+              display: none;
+            }
+            @page {
+              size: B5 landscape;
+              margin: 0; /* Reset margins for the page */
+            }
+            .inner-table th,
+            .inner-table td {
+              padding: 5px;
+              font-size: 10px; 
+            }
+            .hide-scrollbar{
+              overflow-y:hidden;
+            }
+            .inner-table{
+              width:100% ; 
+            }
+          }
+
+          `}
+        </style>
+        <table
+          className="table table-bordered table-styles border-dark"
+          style={{ fontSize: "15px" }}
+        >
+          <tbody>
+            <tr>
+              <td rowSpan="2" colSpan="2">
+                <div className="d-flex flex-column">
+                  <b>Titan Company Limited</b>
+                  <b>Store Address:</b>
+                </div>
+              </td>
+              <td colSpan="3">
+                <div className="d-flex flex-row">
+                  <div className="d-flex flex-column">
+                    <b>Order No: -ACGFRDGG1235</b>
+                    <b>Documnet No: ASDFGHJWERTY54</b>
+                  </div>
+                  <div className="d-flex flex-column mx-5">
+                    <b className="mx-5">Date- 29/08/29</b>
+                    <b className="mx-5">Date- 29/08/29</b>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="3">
+                <div className="d-flex flex-row ">
+                  <div className="d-flex flex-column">
+                    <b>GST NO:- ACGFRDGG1235</b>
+                    <b>State:StateCode:</b>
+                    <b>Place of supply:</b>
+                  </div>
+                  <div className="mx-5">
+                    <b className="mx-5">PAN:- ABCDE1234F</b>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <div className="d-flex flex-row justify-content-between">
+                  <div className="d-flex flex-column">
+                    <b>Bill To & Ship to : </b>
+                    <b>Address 1 : </b>
+                    <b>Address 2 : </b>
+                    <b>State : PinCode : GST No. :</b>
+                    <b>State code : 27</b>
+                  </div>
+                  <div className="d-flex flex-column">
+                    <b> Customer Profile Number : - 784568475846</b>
+                    <b> PAN:CEZPG25447G</b>
+                    <b> Mobile : +91 9500175011</b>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <div className="table-responsive hide-scrollbar">
+                  <table className="table table-bordered inner-table border-dark">
+                    <thead>
+                      <tr>
+                        {WishListHeader.map((heading, i) => {
+                          return <td key={i}>{heading}</td>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>510</td>
+                        <td>Antique</td>
+                        <td>32.581</td>
+                        <td>32.581</td>
+                        <td>paymentDetails</td>
+                        <td>E2507R</td>
+                        <td>4</td>
+                        <td>7000</td>
+                      </tr>
+                      <tr className="text-bold">
+                        <th colSpan="5" className="text-end">
+                          TOTAL
+                        </th>
+                        <th>234</th>
+                        <th>123</th>
+                        <th>345</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="4">
+                <table className="table table-bordered border-dark">
+                  <thead>
+                    <tr>
+                      <th colSpan="5">Payment Details:</th>
+                    </tr>
+                    <tr>
+                      <th scope="col">Payment Mode</th>
+                      <th scope="col">DOC No.</th>
+                      <th scope="col">Date</th>
+                      <th scope="col">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Card</td>
+                      <td>-</td>
+                      <td>21-10-2020</td>
+                      <td>7000</td>
+                    </tr>
+                    <tr>
+                      <td colSpan="3">Total</td>
+                      <td>#Value</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+              <td>
+                <div className="d-flex flex-column">
+                  <b>Total Value</b>
+                  <b>96598.00</b>
+                  <b>IGST @ 0.00</b>
+                  <b>SGST @ 9% </b>
+                  <b>CGST @ 9%</b>
+                  <b>Total order Value:</b>
+                  <b>96598.00</b>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <div className="d-flex flex-row justify-content-around">
+                  <b>Approx security deposite blocked:-</b>
+                  <b>Mode:Card/UPI Card:-Card</b>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <div className="d-flex flex-row justify-content-between">
+                  <b>Expected Date of Delivery:-</b>
+                  <b>Expected Date of Pickup:-</b>
+                  <b>Expected Date of Return pickup:-</b>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <b>Documents to be verified:-</b>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5" className="text-center">
+                We hereby certify that our registration certificate under the
+                Central Goods and Services Tax Act, 2017 is in force on the date
+                on which the supply of the goods/services specified in this tax
+                invoice is made by us and that the transaction of the sale
+                covered by this tax Invoice/ Advance receipt voucher has been
+                effected by us and it shall be accounted for in the turnover of
+                supplies/advances while filing of return and the due tax, if any
+                payable on the supply has been paid or shall be paid
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5">
+                <b>For Titan Company Limited:</b>
+                <p>(Authorized Signatory)</p>
+                <b>Customer Name: Lalitha</b>
+                <p className="mt-2">
+                  Customer Signature: .......................
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5" className="text-center">
+                "Titan Company Limited Regd & Corporate Office: CIN : . Website:
+                www.Titan.Co.in Contact Number: 11111111 Thank you for shopping
+                at Titan Company. You can also write to us at (eMail Id) " E &
+                OE
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="5" className="text-center">
+                For terms and conditions including late fee and product handling
+                or damage charges, please refer: T&C section
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

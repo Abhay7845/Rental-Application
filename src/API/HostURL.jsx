e6f44d726c4cf6078abd117281a65bcc155db807
abhay kumar <abhaykr95678@gmail.com>
2023-10-18 13:30:25 +0530

    updated'

--- src/API/HostURL.jsx
+++ src/API/HostURL.jsx
@@ -1,12 +1,6 @@
 export const HOST_URL =
   "https://tanishqdigitalnpim.titan.in:8443/RentalApplicationUAT/Rental";
 
-// OTP  VERIFICATION URL
-export const Phoneulr1 = "http://10.10.86.11:7001/sms_v1/";
-
-export const Phoneulr2 =
-  "+for+Tanishq+Merchandising+to+approve+the+OTP+request+-+Tanishq&response=Y";
-
 export const UploadImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/";
 export const FetchImg =
   "https://jewbridge.titanjew.in/NPD/api/Docfetch/DownloadImage/?ImageName=";

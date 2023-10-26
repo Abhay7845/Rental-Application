c61aa061c7b5e4cd8f6523ab4e5f88d84e07057d
abhay kumar <abhaykr95678@gmail.com>
2023-10-26 15:00:22 +0530

    updated

--- src/components/Pdf/KarigarQAReturnPdf.jsx
+++ src/components/Pdf/KarigarQAReturnPdf.jsx
@@ -56,35 +56,35 @@ const KarigarQAReturnPdf = (props) => {
             <table
               key={i}
               className="table table-bordered border-dark"
-              style={{ fontSize: "11.5px" }}
+              style={{ fontSize: "11.3px" }}
             >
               <tbody>
                 <tr>
-                  <th>
+                  <th style={{ width: "30%" }}>
                     <div className="d-flex flex-column">
-                      <b>User Name: {storeCode}</b>
-                      <b>SKU Code: {data.itemCode}</b>
-                      <b>Lot Number: {data.lotNo}</b>
+                      <b>User Name:- {storeCode}</b>
+                      <b>SKU Code:- {data.itemCode}</b>
+                      <b>Lot Number:- {data.lotNo}</b>
                     </div>
                   </th>
                   <th>
                     <div className="d-flex flex-column">
                       <b>
-                        Handover Date:-
+                        Handover Date:-{" "}
                         {moment(data.rentStartDate).format("DD-MM-YYYY")}
                       </b>
-                      <b>RSO/Karigar Name: {storeCode}</b>
+                      <b>RSO/Karigar Name:- {storeCode}</b>
                     </div>
                   </th>
                   <th colSpan="2">
                     <div className="d-flex flex-column">
-                      <b>Return Date:-{!returnDate ? "" : returnDate}</b>
-                      <b>RSO/Karigar Name: {storeCode}</b>
+                      <b>Return Date:- {!returnDate ? "" : returnDate}</b>
+                      <b>RSO/Karigar Name:- {storeCode}</b>
                     </div>
                   </th>
                 </tr>
                 <tr>
-                  <th style={{ width: "25%" }}>
+                  <th style={{ width: "30%" }}>
                     <b>Checkpoints </b>
                     <input
                       className="form-check-input mx-4 border-dark"

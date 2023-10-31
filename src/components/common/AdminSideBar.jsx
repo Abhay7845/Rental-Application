8a9e397a6bd0ff3b9624c58f8e938c93d8e97f6a
abhay kumar <abhaykr95678@gmail.com>
2023-10-31 15:30:15 +0530

    updated

--- src/components/common/AdminSideBar.jsx
+++ src/components/common/AdminSideBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../Style/AdminSidebar.css";
 import { Link } from "react-router-dom";
+import brandName from "../../Asset/Img/Tanishq_Logo.png";
 
 const AdminSideBar = () => {
   return (
     <div>
       <div className="AdminSidebarStyle">
+        <img src={brandName} alt="" className="side_brandName" />
         <ul className="mt-3">
           <Link to="/admin/update/master/price" className="NavigationStyle">
             UPDATE ITEM PRICE MASTER

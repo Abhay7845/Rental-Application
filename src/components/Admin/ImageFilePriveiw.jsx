b7915eacf7669d1ef6ab5b693bc8a20a4bd5a4c9
abhay kumar <abhaykr95678@gmail.com>
2023-11-07 12:31:00 +0530

    updated

--- src/components/Admin/ImageFilePriveiw.jsx
+++ src/components/Admin/ImageFilePriveiw.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { ImageFileHeaders, ImageHeaders } from "../../Data/DataList";
+import { useReactToPrint } from "react-to-print";
 import { FetchImg } from "../../API/HostURL";
 import axios from "axios";
 
 const ImageFilePriveiw = ({ previousTnxData, existedUser }) => {
   const docFile = previousTnxData.map((file) => file.paymentDocFileName);
+  const PrintImageRef = useRef(null);
+  const PrintImagePdf = useReactToPrint({
+    content: () => PrintImageRef.current,
+  });
 
   useEffect(() => {
     if (existedUser.addressProofFileName) {
@@ -23,23 +28,33 @@ const ImageFilePriveiw = ({ previousTnxData, existedUser }) => {
   }, [existedUser.addressProofFileName]);
   return (
     <div>
-      <table className="table table-bordered border-dark text-center">
-        <thead className="table-dark border-light">
-          <tr>
-            {ImageFileHeaders.map((heading, i) => {
-              return <td key={i}>{heading}</td>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{existedUser.addressProofFileName}</td>
-            <td>{existedUser.bankDetailFileName}</td>
-            <td>{existedUser.panCardNoFileName}</td>
-            <td>{docFile[0]}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="d-flex justify-content-end my-2">
+        <button className="CButton" onClick={PrintImagePdf}>
+          Print
+        </button>
+      </div>
+      <div className="table-responsive">
+        <table
+          className="table table-bordered border-dark text-center"
+          ref={PrintImageRef}
+        >
+          <thead className="table-dark border-light">
+            <tr>
+              {ImageFileHeaders.map((heading, i) => {
+                return <td key={i}>{heading}</td>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{existedUser.addressProofFileName}</td>
+              <td>{existedUser.bankDetailFileName}</td>
+              <td>{existedUser.panCardNoFileName}</td>
+              <td>{docFile[0]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

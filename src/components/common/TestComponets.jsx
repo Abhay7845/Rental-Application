e4aeb0a4077077e0441def2ae2fa3d860eff758e
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-12-15 16:42:12 +0530

    updatd

--- src/components/common/TestComponets.jsx
+++ src/components/common/TestComponets.jsx
@@ -1,53 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import shopBag from "../../Asset/Img/shopBag.png"
+import '../../Style/ShopBagAnimate.css'
 
 
 const TestComponets = () => {
 
-  // Use state to manage checkbox states for each row
-  const [checkboxes, setCheckboxes] = useState({});
-
-  // Handle checkbox change for a specific row
-  const handleCheckboxChange = (rowId) => {
-    setCheckboxes((prevCheckboxes) => ({
-      ...prevCheckboxes,
-      [rowId]: !prevCheckboxes[rowId], // Toggle the checkbox state for the specific row
-    }));
-  };
-  console.log("checkboxes==>", checkboxes)
-  // Sample data for the table
-  const tableData = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-  ];
-
-
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Checkbox</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>
-                <input
-                  type="checkbox"
-                  checked={checkboxes[item.id] || false}
-                  onChange={() => handleCheckboxChange(item.id)}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="shopBagStyle">
+      <div class="steam" id="steam1"> </div>
+      <div class="steam" id="steam2"> </div>
+      <div class="steam" id="steam3"> </div>
+      <div class="steam" id="steam4"> </div>
+
+      {/* <div id="cup">
+          <div id="cup-body">
+            <div id="cup-shade"></div>
+          </div>
+          <div id="cup-handle"></div>
+        </div> */}
+
+      <img src={shopBag} id="saucer" />
+      {/* <div id="shadow"></div> */}
     </div>
   );
 };

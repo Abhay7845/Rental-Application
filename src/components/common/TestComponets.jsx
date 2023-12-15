8a1006bd752cb6095713da94c18861b8444f35ce
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-12-15 17:02:18 +0530

    updatd

--- src/components/common/TestComponets.jsx
+++ src/components/common/TestComponets.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import shopBag from "../../Asset/Img/shopBag.png"
 import '../../Style/ShopBagAnimate.css'
+import earRing from "../../Asset/Img/EarRing.png"
+import braslet from "../../Asset/Img/Brasslet.png"
+import neckLess from "../../Asset/Img/NeckLess.png"
+import fingerRing from "../../Asset/Img/FingerRing.png"
 
 
 const TestComponets = () => {
 
   return (
     <div className="shopBagStyle">
-      <div class="steam" id="steam1"> </div>
-      <div class="steam" id="steam2"> </div>
-      <div class="steam" id="steam3"> </div>
-      <div class="steam" id="steam4"> </div>
+      <img src={earRing} class="steam" id="steam1" />
+      <img src={braslet} class="steam" id="steam2" />
+      <img src={neckLess} class="steam" id="steam3" />
+      <img src={fingerRing} class="steam" id="steam4" />
 
       {/* <div id="cup">
           <div id="cup-body">

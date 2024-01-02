5bbd3d83a6fb651a1d3fd8e859e224439b83b6b4
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-01-02 11:49:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./user/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/RSO/RsoHome";

df7ae002ee95bb321abbc95b8a962add20a60ba1
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-02-12 16:07:46 +0530

    updated

--- src/components/common/TestComponets.jsx
+++ src/components/common/TestComponets.jsx
@@ -15,26 +15,18 @@ const TestComponets = () => {
       <Table className="table table-bordered border-dark">
         <Thead>
           <Tr>
-            <Th>Event</Th>
-            <Th>Date</Th>
-            <Th>Location</Th>
+            <Th>Image</Th>
+            <Th>Image Name</Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
             <Td>Tablescon</Td>
             <Td>9 April 2019</Td>
-            <Td>East Annex</Td>
           </Tr>
           <Tr>
             <Td>Capstone Data</Td>
             <Td>19 May 2019</Td>
-            <Td>205 Gorgas</Td>
-          </Tr>
-          <Tr>
-            <Td>Tuscaloosa D3</Td>
-            <Td>29 June 2019</Td>
-            <Td>Github</Td>
           </Tr>
         </Tbody>
       </Table>

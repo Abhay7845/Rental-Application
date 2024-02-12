a6b619c1984d56c5af3364f6445bc7c207025533
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-02-12 16:22:12 +0530

    updated

--- src/components/common/TestComponets.jsx
+++ src/components/common/TestComponets.jsx
@@ -4,14 +4,27 @@ import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 
 
 const TestComponets = () => {
+  const downloadImage = (imageUrl) => {
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = imageUrl;
+    link.download = 'image.png';
 
+    // Simulate a click event to trigger the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const imgObj = [
+    { id: "1", imgUrl: "https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { id: "2", imgUrl: "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" },
+  ]
   return (
     <div className="mx-2">
       <br />
       <br />
-      <br />
-      <br />
-      <br />
+      {/* <button onClick={downloadImage}>Click</button> */}
       <Table className="table table-bordered border-dark">
         <Thead>
           <Tr>
@@ -20,14 +33,13 @@ const TestComponets = () => {
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>Tablescon</Td>
-            <Td>9 April 2019</Td>
-          </Tr>
-          <Tr>
-            <Td>Capstone Data</Td>
-            <Td>19 May 2019</Td>
-          </Tr>
+          {imgObj.map((item, i) => {
+            return (
+              <Tr key={i}>
+                <Td>Tablescon</Td>
+                <Td><img src={item.imgUrl} onClick={() => downloadImage(item.imgUrl)} alt="Image_1" height="100" width="100" /></Td>
+              </Tr>)
+          })}
         </Tbody>
       </Table>
     </div>

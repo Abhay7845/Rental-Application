1fd52aff4156b7784adcd2c6365b162f32bc03cb
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-02-29 09:45:37 +0530

    updated

--- src/API/HostURL.jsx
+++ src/API/HostURL.jsx
@@ -1,14 +1,14 @@
 
 // PRODUCTION URL
-export const HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/RentalApplication/Rental";
-export const UploadImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/";
-export const FetchImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/DownloadImage/?ImageName=";
+// export const HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/RentalApplication/Rental";
+// export const UploadImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/";
+// export const FetchImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/DownloadImage/?ImageName=";
 
 
 // UAT URL
-// export const HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/RentalApplicationUAT/Rental";
-// export const UploadImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/JewrentalUat";
-// export const FetchImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/UatDownloadImage/?ImageName=";
+export const HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/RentalApplicationUAT/Rental";
+export const UploadImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/JewrentalUat";
+export const FetchImg = "https://jewbridge.titanjew.in/NPD/api/Docfetch/UatDownloadImage/?ImageName=";
 
 
 export const thresholdmsg = "Cannot issue the products due to high outstanding of products under rent. Kindly return the previously rented products to take delivery of the currently booked items.";
